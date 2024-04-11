14e01b19ed70ba498d6f8a5d28103b18fd5ed86b
Michelsom Pereira <michelsonpagani@gmail.com>
2024-04-10 21:32:07 -0400

    feat: add new icons and add settings about icons

---
+++ src/@types/dec.d.ts
@@ -0,0 +1,20 @@
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
+declare module '*.gif';
+
+declare module '*.svg' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: any;
+  export default value;
+}
---
+++ src/Assets/index.ts
@@ -0,0 +1,3 @@
+export {default as IsNotVisible} from './eye-off.svg';
+export {default as IsVisible} from './eye.svg';
+export {default as VectorX} from './vector-x.svg';
