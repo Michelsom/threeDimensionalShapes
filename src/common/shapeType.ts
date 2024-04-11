567729fce26e5bc860bf47dddb8af58e6027a4ac
Michelsom Pereira <michelsonpagani@gmail.com>
2024-04-10 21:32:34 -0400

    feat: add new global datas

---
+++ src/common/shapeType.ts
@@ -0,0 +1,5 @@
+export const selectShape: Record<number, string> = {
+  1: 'cube',
+  2: 'cone',
+  3: 'dodecahedron',
+};
---
+++ src/common/velocity.ts
@@ -0,0 +1,5 @@
+export const velocity: Record<number, number> = {
+  0.01: 0.5,
+  0.04: 1.0,
+  0.07: 1.5,
+};
