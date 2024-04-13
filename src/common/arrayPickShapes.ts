05278dffc01ac77d62c537cb5ed7002a1d1fa222
Michelsom Pereira <michelsonpagani@gmail.com>
2024-04-12 20:24:02 -0400

    feat: create new mock

---
+++ src/common/arrayPickShapes.ts
@@ -0,0 +1,14 @@
+export const shapeTypes = [
+  {
+    id: 1,
+    shapeType: 1,
+  },
+  {
+    id: 2,
+    shapeType: 2,
+  },
+  {
+    id: 3,
+    shapeType: 3,
+  },
+];
---
+++ src/common/arrayVelocity.ts
@@ -0,0 +1,14 @@
+export const pickVelocity = [
+  {
+    id: 1,
+    rotation: 0.01,
+  },
+  {
+    id: 2,
+    rotation: 0.04,
+  },
+  {
+    id: 3,
+    rotation: 0.07,
+  },
+];
