4d960f802ebdbc0122505b6021817667379f8dde
Michelsom Pereira <michelsonpagani@gmail.com>
2024-04-12 20:15:48 -0400

    feat: add new icons

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -1,3 +1,4 @@
+export {default as ArrowBlack} from './arrow_back.svg';
 export {default as IsNotVisible} from './eye-off.svg';
 export {default as IsVisible} from './eye.svg';
 export {default as VectorX} from './vector-x.svg';
