5e5009dedf52e847950dc7c81bd888ef1acae86f
Michelsom Pereira <michelsonpagani@gmail.com>
2024-04-12 20:19:04 -0400

    feat: create Render empty component

---
+++ src/screens/home/components/RenderEmptyComponent/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ContentEmptyComponent, TextAddShape } from "./styles";
+
+export const RenderEmptyComponent = () => (
+  <ContentEmptyComponent>
+    <TextAddShape>
+      Você não possui nenhuma forma
+    </TextAddShape>
+  </ContentEmptyComponent>
+);
---
+++ src/screens/home/components/RenderEmptyComponent/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const ContentEmptyComponent = styled.TouchableOpacity`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+export const TextAddShape = styled.Text`
+  color: #212121;
+  font-size: 16px;
+  font-weight: 600;
+`;
