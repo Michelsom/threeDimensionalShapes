f09a5bee6a23f8f52fc3197693c57ffbb2318c62
Michelsom Pereira <michelsonpagani@gmail.com>
2024-04-12 20:16:04 -0400

    feat: delete

--- src/Components/BoxScene/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {GlobalScene} from './styles';
-import {useViewModelScene} from './viewModel';
-export const BoxScene = () => {
-  const {onGLContextCreate} = useViewModelScene();
-  return <GlobalScene onContextCreate={onGLContextCreate} />;
-};
--- src/Components/BoxScene/styles.ts
+++
@@ -1,6 +0,0 @@
-import {GLView} from 'expo-gl';
-import styled from 'styled-components/native';
-
-export const GlobalScene = styled(GLView)`
-  flex: 1;
-`;
